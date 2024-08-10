53e277d495f75773524522b1efa6d907031ac4ce
pouya ghajardavalou <pouyaximac@pouyas-iMac.local>
2024-08-10 23:26:27 +0200

    improved high and width identifier

--- map_validating.c
+++ map_validating.c
@@ -1,36 +1,81 @@
-
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "get_next_line.h"
 
-
-int ft_char_counter(char *str, int size)
+int	ft_char_counter(char *str, int size)
 {
-    int i;
-    
-    i = 0;
-    while (str[i] && str[i] != '\n')
-        i++;
-    
-    // Check if the length of the string exceeds 22 characters
-    if (i > size)
-        return 0; // Return 0 if the length exceeds the limit of 22 characters
-    return 1;     // Return 1 if the length is within the limit
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (i > size)
+		return (0);
+	return (1);
 }
 
-int main (int ac, char **av)
+void	check_line_len(char *str, int size)
 {
-    int fd;
-    
-    if (ac != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    fd = open(av[1], O_RDONLY);
-    if ( fd < 0)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    close(fd);
+	char	*line;
+	int		fd;
+
+	fd = open(str, O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Error");
+		exit (EXIT_FAILURE);
+	}
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		if (!ft_char_counter(line, size))
+		{
+			free(line);
+			close(fd);
+			printf("Line exceeds characters\n");
+			exit (EXIT_FAILURE);
+		}
+		free(line);
+	}
+	close(fd);
+}
+
+void	max_line(char *str, int size)
+{
+	char *line;
+	int	fd;
+	int i;
+
+	i = 0;
+	fd = open(str, O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Error");
+		return ;
+	}
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		i++;
+	}
+	free(line);
+	close(fd);
+	if (i > size)
+	{
+		printf("Height exceeds characters\n");
+		exit (1);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int size = 5;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	check_line_len (av[1], size);
+	max_line(av[1], size);
 }
