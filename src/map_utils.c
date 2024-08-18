db985f346d73da115faf36767c7c2a8a83e5e938
Pouya Ghajardavalou <75066687+with-pouyax@users.noreply.github.com>
2024-08-18 23:02:58 +0200

    added a comment as a reminder

--- src/map_utils.c
+++ src/map_utils.c
@@ -6,7 +6,7 @@
 /*   By: pghajard <pghajard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 14:27:36 by pghajard          #+#    #+#             */
-/*   Updated: 2024/08/18 22:53:25 by pghajard         ###   ########.fr       */
+/*   Updated: 2024/08/18 23:01:44 by pghajard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,42 +49,66 @@ char	**expand_map(t_map_info *info)
 	return (new_map);
 }
 
-char	**read_map(int fd, t_count *counts, int *width, int *height)
+void	read_and_process_map(int fd, t_count *counts, int *width, int *height)
 {
-	t_map_info	info;
-	char		c;
-	int			line_len;
-
-	info.map_capacity = INITIAL_MAP_SIZE;
-	info.map = malloc(info.map_capacity * sizeof(char *));
-	info.height = 0;
-	info.fd = fd;
-	if (!info.map)
-		exit_with_error33("Error: Memory allocation failed", fd);
+	char	c;
+	int		line_len;
+	int		line_num;
 
 	line_len = 0;
+	line_num = 0;
+
 	while (read(fd, &c, 1) == 1)
 	{
 		if (c == '\n')
 		{
-			info.map[info.height][line_len] = '\0';
-			if (info.height == 0)
+			if (line_num == 0)
 				*width = line_len;
-			info.height++;
+			else if (line_len != *width)
+				exit_with_error33("Error: Map is not rectangular", fd, NULL);
+
+			line_num++;
 			line_len = 0;
-			if (info.height >= info.map_capacity)
-				info.map = expand_map(&info);
 		}
 		else
 		{
-			if (line_len == 0)
-				info.map[info.height] = malloc((*width + 1) * sizeof(char));
-			info.map[info.height][line_len++] = c;
-			if (c == 'P' || c == 'C')
-				track_map_info(info.map[info.height], counts, &info.height);
+			if (line_num == 0)
+				line_len++;
+			else if (line_len >= *width)
+				exit_with_error33("Error: Line length exceeds expected width", fd, NULL);
+
+			// Process the character directly
+			if (c == 'P')
+			{
+				if (counts->p_x != -1 || counts->p_y != -1)
+					exit_with_error33("Error: Multiple player start positions found", fd, NULL);
+				counts->p_x = line_len;
+				counts->p_y = line_num;
+				counts->p_count++;
+			}
+			else if (c == 'C')
+				counts->c_count++;
+			else if (c == 'E')
+				counts->e_count++;
+			else if (c != '0' && c != '1')
+				exit_with_error33("Error: Invalid character in map", fd, NULL);
+
+			line_len++;
 		}
 	}
 
-	*height = info.height;
-	return (info.map);
+	if (line_len != 0)
+	{
+		if (line_len != *width)
+			exit_with_error33("Error: Last line is not rectangular", fd, NULL);
+		line_num++;
+	}
+
+	*height = line_num;
+
+	// Additional validation can go here if necessary
+	if (counts->p_count != 1 || counts->c_count < 1 || counts->e_count != 1)
+		exit_with_error33("Error: Invalid map composition", fd, NULL);
 }
+
+////// you can do read_and_process_map without malloc I guess. 
