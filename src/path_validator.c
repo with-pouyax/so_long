ec075a4e257d981f901a123b568022af2ef4468b
Pouya Ghajardavalou <75066687+with-pouyax@users.noreply.github.com>
2024-08-26 13:11:52 +0200

    splited read map into smaller functions

--- src/path_validator.c
+++ src/path_validator.c
@@ -6,7 +6,7 @@
 /*   By: pghajard <pghajard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 13:45:54 by pghajard          #+#    #+#             */
-/*   Updated: 2024/08/26 13:03:23 by pghajard         ###   ########.fr       */
+/*   Updated: 2024/08/26 13:09:14 by pghajard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,42 @@ static void	track_map_info(char c, int x, int y, t_count *counts)
 		counts->exit_found = 1;
 }
 
+static char	*allocate_map_line(int y, int width)
+{
+	char	*line;
+
+	if (y == 0)
+		line = malloc((MAX_WIDTH + 1) * sizeof(char));
+	else
+		line = malloc((width + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	return (line);
+}
+
+static void finalize_line(char **map, int *line_len, int *y, int *width)
+{
+	map[*y][*line_len] = '\0';
+	if (*y == 0)
+		*width = *line_len;
+	(*y)++;
+	*line_len = 0;
+}
+
+static int process_char(char c, char **map, int *line_len, int y, t_count *counts, int width)
+{
+	if (*line_len == 0)
+	{
+		map[y] = allocate_map_line(y, width);
+		if (!map[y])
+			return (-1);
+	}
+	map[y][*line_len] = c;
+	track_map_info(c, *line_len, y, counts);
+	(*line_len)++;
+	return (0);
+}
+
 char	**read_map(int fd, t_count *counts, int *width, int *height)
 {
 	char	**map;
@@ -81,40 +117,19 @@ char	**read_map(int fd, t_count *counts, int *width, int *height)
 	while (read(fd, &c, 1) == 1)
 	{
 		if (c == '\n')
+			finalize_line(map, &line_len, &y, width);
+		else if (process_char(c, map, &line_len, y, counts, *width) == -1)
 		{
-			map[y][line_len] = '\0';
-			if (y == 0)
-				*width = line_len;
-			y++;
-			line_len = 0;
-		}
-		else
-		{
-			if (line_len == 0)
-			{
-				if (y == 0)
-					map[y] = malloc((MAX_WIDTH + 1) * sizeof(char));
-				else
-					map[y] = malloc((*width + 1) * sizeof(char));
-				if (!map[y])
-				{
-					free_map_memory(map, y);
-					exit_with_error33("Error: Memory allocation failed", fd);
-				}
-			}
-			map[y][line_len] = c;
-			track_map_info(c, line_len, y, counts);
-			line_len++;
+			free_map_memory(map, y);
+			exit_with_error33("Error: Memory allocation failed", fd);
 		}
 	}
 	if (line_len != 0)
-	{
-		map[y][line_len] = '\0';
-		y++;
-	}
+		finalize_line(map, &line_len, &y, width);
 	*height = y;
-	return map;
+	return (map);
 }
+
 
 
 void validate_map_path(char *filename)
